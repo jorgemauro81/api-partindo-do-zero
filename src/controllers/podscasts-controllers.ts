d58023684f4f2f2940d7b04aed4164345ae65783
jorgemaurobs <jorgemaurobs@gmail.com>
2026-06-28 17:38:58 -0300

    Alteração nos objetos para finalizar

--- src/controllers/podscasts-controllers.ts
+++ src/controllers/podscasts-controllers.ts
@@ -3,6 +3,7 @@ import { servicesListEpisodes } from "../services/list-episodes-services";
 import { servicesFilterEpisodes } from "../services/filter-episodes-services";
 import { StatusCode } from "../utils/status-code";
 import { ContentType } from "../utils/content-type";
+import { IFilterPodsCasts } from "../interfaces/IFilter-Podscasts";
 
 export const getListEpisodes = async (
   req: IncomingMessage,
@@ -17,8 +18,9 @@ export const getFilterEpisodes = async (
   req: IncomingMessage,
   res: ServerResponse,
 ) => {
-  res.writeHead(StatusCode.OK, { "Content-Type": ContentType.JSON });
+  const Resp: IFilterPodsCasts = await servicesFilterEpisodes(req.url);
+  res.writeHead(Resp.statusCode, { "Content-Type": ContentType.JSON });
+  const Response = Resp.body;
 
-  const Response = await servicesFilterEpisodes(req.url);
   res.end(JSON.stringify({ Response }));
 };
